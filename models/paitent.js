7cb2c9a7c230f000743246c752538f36324eb051
Daniel Tabaja <daniel.tabaja@s.afeka.ac.il>
2021-03-21 11:01:07 +0200

    Access array in paitent began

--- models/paitent.js
+++ models/paitent.js
@@ -1,51 +1,52 @@
-mongoose=require("mongoose");
-var passportLocalMongoose=require("passport-local-mongoose");
+mongoose = require("mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 
-mongoose=require("mongoose");
+mongoose = require("mongoose");
 const { Collection } = require("mongoose");
-var passportLocalMongoose=require("passport-local-mongoose");
+var passportLocalMongoose = require("passport-local-mongoose");
 
 //define User Entity
 //need to decide if there is username
 
-const ReportSchema=new mongoose.Schema({
-  stageLevel:Number,
-  exerciseLevel:Number,
-  questions:[{
-    type:String
-  }],
-  answers:[{
-    type:Number
-  }],
-  openAnswer:String,
-  totalScore:Number
+const ReportSchema = new mongoose.Schema({
+    stageLevel: Number,
+    exerciseLevel: Number,
+    questions: [{
+        type: String
+    }],
+    answers: [{
+        type: Number
+    }],
+    openAnswer: String,
+    totalScore: Number
 });
 
-var PaitentSchema=new mongoose.Schema({
-  username:String,
-  password:String,
-  name:String,
-  protocol:{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Protocol'
-  },
-  
-  treatmentTypes:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'TreatmentType'
-  }],
-     reports:[ReportSchema],
-    
-   /*
-  scoreList:[{
-    type:mongoose.Schema.Types.ObjectId,
-    ref:'Score'
+
+var AccessSchema = new mongoose.Schema({
+    stageBool: Boolean,
+    exerciseBool: [{ type: Boolean }]
+
+});
+
+var PaitentSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    protocol: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Protocol'
+    },
+
+    treatmentTypes: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'TreatmentType'
     }],
-   */
-   },
-  {collection : ' Paitents'});
-  //need to add more attr here! highRecord,ScoreList,Access
+
+    accesses: [AccessSchema],
+    reports: [ReportSchema],
+}, { collection: ' Paitents' });
+//need to add more attr here! highRecord,ScoreList,Access
 
 PaitentSchema.plugin(passportLocalMongoose);
-module.exports=mongoose.model("Paitent",PaitentSchema);
+module.exports = mongoose.model("Paitent", PaitentSchema);
